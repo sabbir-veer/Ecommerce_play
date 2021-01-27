343da67522c277ce0bd4d2b1acec8d68c802971e
Sabbir Ahmed <sabbir.hstu.19@gmail.com>
2021-01-27 20:05:20 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const card = document.querySelector(".card");
+card.addEventListener("mouseover", mouseOver);
+function mouseOver() {
+  //   document.querySelector(".name").style.opacity = 1;
+  //   document.querySelector(".spec").style.opacity = 1;
+}
